6a7a3dc06cda29c9f4a92889a08b513051b14a65
Shaojie Li <shaojiel@vmware.com>
2025-01-08 18:28:13 +0800

    Init

---
+++ src/main/java/cn/shaojiel/junittestng/JunitTestngApplication.java
@@ -0,0 +1,13 @@
+package cn.shaojiel.junittestng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitTestngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/shaojiel/junittestng/JunitTestngApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.shaojiel.junittestng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
